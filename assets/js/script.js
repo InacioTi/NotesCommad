84712c0f424a8df548b834cb324339717526ae1f
Jose Inacio <38287810+InacioTi@users.noreply.github.com>
2023-01-12 09:38:29 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -155,13 +155,11 @@ var data = [
 
 //Crontab
 {"title":"List cron jobs: crontab -l"},
-{"title":"Edit cron job: crontab -e"},
+{"title":"Edit cron job: crontab -e"}
 
 ]
 
-
 //O trecho de código acima corresponde a uma função que é executada quando o usuário digita algo no campo de busca. A função começa por definir duas variáveis, count e t0 e t1, para armazenar os resultados da pesquisa. A variável count é usada para contar o número de resultados encontrados. As variáveis t0 e t1 são usadas para medir o tempo de processamento da pesquisa. Em seguida, é criado um regex para procurar o termo de pesquisa nos dados. Depois disso, é percorrido cada item de dados e procurado os termos de pesquisa. Se achado, o item é adicionado ao array de resultados. Por fim, o resultado é mostrado na tela junto com o número de itens encontrados e o tempo de processamento da pesquisa.
-
 var count = 0;
 var t0 = t1 = 0;
 
@@ -179,7 +177,8 @@ $('#txt-search').keyup(function() {
     $.each(data, function(key, val) {
         if ((val.title.search(regex) != -1) || (val.link.search(regex) != -1)) {
             output += `<div class="row antialiased p-2">
-          <a ${val.link} target="_blank" rel="noopener nofollow noreferrer"> ✍️ ${val.title}</a>
+          <a href=${val.link} target="_blank" rel="noopener nofollow noreferrer">
+✍️ ${val.title}</a>
                       </div>`
             cresult.push({
                 title: `${val.title}`,
@@ -195,9 +194,10 @@ $('#txt-search').keyup(function() {
     $("#filter-records").prepend(`<div class="text-bold text-center mb-8">Found <code>${cresult.length}</code> resources related to <b>${removeTags(searchField)}</b> in <code>${t1-t0} ms</code></div>`);
 });
 
-// hashtag clicks 
+/ hashtag clicks 
 //O trecho de código acima trata de um evento de clique, onde quando o elemento com a classe .searchfilter for clicado, é executada a função search(), que verifica se o conteúdo do elemento contém o valor passado como parâmetro no elemento. Se o resultado for positivo, é adicionado um elemento com um link e o título ao elemento #filter-records.
 //Também é feita uma função que remove os banners presentes na página, outra para verificar parâmetros da url, uma função para converter o resultado em um objeto json e outra para sanitizar o html.
+
 
 $('.searchfilter').click(function() {
     output = search($(this).val());
@@ -252,22 +252,22 @@ function jsonify(filter) {
 
 // JSON 
  function downloadObjectAsJson(filter){
-    if($('#txt-search').val().length > 0) {
-        var exportData = jsonify($('#txt-search').val())
-        var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportData));
-        var downloadAnchorNode = document.createElement('a');
-        downloadAnchorNode.setAttribute("href",     dataStr);
-        downloadAnchorNode.setAttribute("download", `${$('#txt-search').val()}.json` );
-        document.body.appendChild(downloadAnchorNode); // required for firefox
-        downloadAnchorNode.click();
-        downloadAnchorNode.remove();
-    }
+	if($('#txt-search').val().length > 0) {
+		var exportData = jsonify($('#txt-search').val())
+		var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportData));
+		var downloadAnchorNode = document.createElement('a');
+		downloadAnchorNode.setAttribute("href",     dataStr);
+		downloadAnchorNode.setAttribute("download", `${$('#txt-search').val()}.json` );
+		document.body.appendChild(downloadAnchorNode); // required for firefox
+		downloadAnchorNode.click();
+		downloadAnchorNode.remove();
+	}
   }
   
  
  // Sanitize Html
  
-var tagBody = '(?:[^"\'>]|"[^"]*"|\'[^\']*\')*';
+ var tagBody = '(?:[^"\'>]|"[^"]*"|\'[^\']*\')*';
 
 var tagOrComment = new RegExp(
     '<(?:'
@@ -281,7 +281,7 @@ var tagOrComment = new RegExp(
     + tagBody
     + ')>',
     'gi');
-    
+	
 function removeTags(html) {
   var oldHtml;
   do {
